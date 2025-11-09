79bf8a7a1e3806c002e9aea7e032433f182a4ecf
vardhireddysaivardhanreddy-hash <vardhireddysaivardhanreddy@gmail.com>
2025-11-09 23:48:00 +0530

    updated pass and user

--- library.py
+++ library.py
@@ -4,8 +4,8 @@ from datetime import date
 # Connect to MySQL
 conn = mysql.connector.connect(
     host="localhost",
-    user="root",        # change if needed
-    password="Varuntej@2003",        # enter your MySQL password
+    user="appuser",
+    password="password123",       # enter your MySQL password
     database="Library"
 )
 cur = conn.cursor()
